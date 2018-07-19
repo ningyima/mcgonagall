e6e49269540561f0a33194880c40a8bab8f720cb
Vernon E. Neilly, II <28078313+vneilly2@users.noreply.github.com>
2018-07-18 22:37:18 -0700

    updated 'db.js' file // MongoDB 'test' is connecting + able to write to DB + able to retrieve user via query // minor issues on retrieve user by find query // updated server.js file

---
+++ database/db.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/test?connectTimeoutMS=5000&bufferCommands=false')
+  .then(
+    () => { console.log('mongoose connected'); },
+    (err) => { console.log('DB connection error: ', err); },
+  );
+
+const db = mongoose.connection;
+
+const userSchema = mongoose.Schema({
+  // email: { type: String, required: true, unique: true },
+  // name: { type: String },
+  // tokenSeed: { type: String, required: true, unique: true },
+  // created: { type: Date, default: () => new Date() },
+  email: String,
+  name: String,
+  tokenSeed: String,
+  created: Date,
+});
+
+let Users = mongoose.model('User', userSchema);
+const findAllUsers = function (callback) {
+  Users.find({}, (err, users) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, users);
+    }
+  }); 
+};
+
+const checkUser = (userId, callback) => {
+  // construct query to pass to DB
+  Users.find({ _id: '5b501b167a41dc2ff95047ed' }, (err, user) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, user);
+      console.log('success user found');
+    }
+  });
+  // connect to DB and make query.
+
+  // run callback with results
+};
+
+module.exports.checkUser = checkUser;
+module.exports.findAllUsers = findAllUsers;
+module.exports.Users = Users;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('../database/db');
 
 const app = express();
+const router = express.Router();
+
+
 app.engine('html', require('ejs').renderFile);
 
-app.set('views', __dirname + '/../src');
+app.set('views', `${__dirname}/../src`);
 app.set('view engine', 'html');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true,
@@ -13,6 +18,38 @@ app.use(bodyParser.urlencoded({
 
 app.get('/', (req, res) => {
   res.render('index');
+  console.log('inside index');
+});
+
+app.get('/login/write', (req, res) => {
+  // res.send('hello world');
+  // console.log('inside login/write');
+
+//   const Users = new db.Users({
+//  email: 'test3@email.com', name: 'MyTest3', tokenSeed: 'anb9uhidjjkgjkjeihijgi', created: '2017-04-12 00:00:00.000' 
+// } );
+//   Users.save().then((user) => {
+//     res.send(`${user} saved to mongoDB`);
+//   }).catch((err) => {
+//     res.sendStatus(400).send('unable to save to DB');
+//   });
+  db.checkUser('5b501b167a41dc2ff95047ed', (err, user) => {
+    if (err) {
+      res.send(err);
+    } else {
+      console.log('user found');
+      res.send(user);
+    }
+  });
+  // db.findAllUsers((err, users) => {
+  //   if (err) {
+  //     console.log('returned from findAllUsers', err);
+  //     res.sendStatus(404);
+  //   } else {
+  //     console.log(users);
+  //     res.send(users);
+  //   }
+  // });
 });
 
 const port = 3000;
