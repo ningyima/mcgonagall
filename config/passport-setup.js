e522a55951301debfac7f07e505c5ef873d36324
Vernon E. Neilly, II <28078313+vneilly2@users.noreply.github.com>
2018-07-21 02:31:49 -0700

    GoogleStrategy and credentials setup passport-setup.js

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,9 +1,12 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth');
+const GoogleOAuth = require('../config');
 
 passport.use(
   new GoogleStrategy({
     // options for GoogleStrategy
+    clientID: GoogleOAuth.googlecredential.clientID,
+    clientSecret: GoogleOAuth.googlecredential.clientSecret,
   }), () => {
     // passport cb function
   },
