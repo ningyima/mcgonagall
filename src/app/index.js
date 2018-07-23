1495ef86e27b41e1aaf4c5efa81e29ea7f4e0d36
Alicia Nagee <adnagee@Alicias-MacBook-Pro.local>
2018-07-23 17:05:20 -0400

    minor formatting changes

--- src/app/index.js
+++ src/app/index.js
@@ -235,8 +235,12 @@ const HomepageLayout = () => (
     <Segment style={{ padding: '8em 0em' }} vertical>
       <Grid container stackable verticalAlign='middle'>
         <Grid.Row>
-          <Grid.Column width={8}> 
+          <Grid.Column width={12}> 
             <SearchApiForm />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column width={8}> 
             <Header as='h3' style={{ fontSize: '2em' }}>
               Healthier, Informed, Affordable meal prep.
             </Header>
