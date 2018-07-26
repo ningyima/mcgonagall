4ddd0a60ec8703fad02c89bd218b460ab7f32955
Seth Jetro <jsethjetro@gmail.com>
2018-07-26 17:11:27 -0500

    display with fake data

--- src/app/recipeEntry.js
+++ src/app/recipeEntry.js
@@ -1,5 +1,7 @@
 const RecipeEntry = ({recipe}) => (
-
+  <div><div><Image wrapped size='medium' src={recipe.image}/></div>
+    <span>{recipe.title}</span>
+  </div>
 )
 
 export default RecipeEntry;
--- src/app/recipes.js
+++ src/app/recipes.js
@@ -1,6 +1,43 @@
-var RecipeList = (props) => (
-  <div className="">
-    {props.recipes.map(el =>
-      <RecipeEntry recipe={el}/>)}
-  </div>
-);
+import React, { Component } from 'react';
+import HomepageLayout from './index';
+
+import { Button, Header, Image, Modal } from 'semantic-ui-react';
+//import HomepageLayout from './index';
+
+class RecipeList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+
+  handleRef(component) {
+    this.ref = component;
+  }
+
+  render() {
+    console.log('we have value: ',this.props);
+    return(
+      <div className="">
+        <Modal open={this.props.open} onClose={()=>{this.props.close()}} closeIcon>
+          <Modal.Header>Ingredients lists</Modal.Header>
+            {this.props.recipes.map((recipe)=>{
+                return <Modal.Content image>
+                  <Image wrapped size='medium' src={recipe.image} />
+                  <Modal.Description>
+                    <Header>{recipe.title}</Header>
+                    <p>Good flavor</p>
+                    <p>Let budgetLife be your guide</p>
+                  </Modal.Description>
+                </Modal.Content>
+             })}
+        </Modal>
+      </div>
+    )
+  }
+}
+export default RecipeList;
+
+
+
