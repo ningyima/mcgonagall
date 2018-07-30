f17acdff1cc85e5742182828c060170842aeb597
Vernon E. Neilly, II <28078313+vneilly2@users.noreply.github.com>
2018-07-30 12:57:11 -0700

    redo login.js + signup.js edits

--- src/app/signup.js
+++ src/app/signup.js
@@ -51,14 +51,15 @@ class ModalSignupForm extends Component {
        </Modal.Description>
        </Modal.Content>
        <Modal.Content>
-          <Button color='facebook' onClick={() => {this.props.fbHandler();}} >
+          <a href='auth/facebook' >
+          <Button color='facebook' >
             <Icon name='facebook' /> Facebook
-          </Button>
-          <Button color='google plus' onClick={() => {this.props.googleHandler();}}>
+          </Button> </a>
+          <a href='auth/google' >
+          <Button color='google plus'>
             <Icon name='google plus' /> Google Plus
-          </Button>
+          </Button> </a>
         </Modal.Content>
-       <Modal.Content>
       <Form>
         <div className="two fields"> 
           <Form.Field>
