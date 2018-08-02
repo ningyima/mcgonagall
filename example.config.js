b5807c3daf35601156ebacba5e82abbdab8ffeb1
Maureen and John McCurley <maureenandjohn@Maureens-iMac.local>
2018-08-02 18:30:27 -0400

    Added dependencies and schema/config basics

--- example.config.js
+++ example.config.js
@@ -1,23 +1,23 @@
-const config = {
-    mlabdb: {
-      mlab: ‘’,
-      mongod: `mongodb://localhost/recipetest`
-    },
-    API_KEYS: {
-      ‘X-Mashape-Key’: 'RECIPE-FILL-IN',
-      ‘X-Mashape-Host’: 'RECIPE-FILL-IN'
-    },
-    googlecredential: {
-      clientID: 'FILL-IN',
-      clientSecret: 'FILL-IN'
-    },
-    facebookcredential: {
-      clientID: 'FILL-IN',
-      clientSecret: 'FILL-IN'
-    },
-    session: {
-      cookieKey: 'FILL-IN'
-    }
-   };
+// const config = {
+//     mlabdb: {
+//       mlab: ‘’,
+//       mongod: `mongodb://localhost/recipetest`
+//     },
+//     API_KEYS: {
+//       ‘X-Mashape-Key’: 'RECIPE-FILL-IN',
+//       ‘X-Mashape-Host’: 'RECIPE-FILL-IN'
+//     },
+//     googlecredential: {
+//       clientID: 'FILL-IN',
+//       clientSecret: 'FILL-IN'
+//     },
+//     facebookcredential: {
+//       clientID: 'FILL-IN',
+//       clientSecret: 'FILL-IN'
+//     },
+//     session: {
+//       cookieKey: 'FILL-IN'
+//     }
+//    };
    
-module.exports = config;
+// module.exports = config;
