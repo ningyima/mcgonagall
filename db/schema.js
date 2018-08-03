fcef23f444021ba053b47af77a82f9d5f62be17f
darthsticky <tristancarter@gmail.com>
2018-08-02 20:00:32 -0400

    Fixed Linting Errors

--- db/schema.js
+++ db/schema.js
@@ -1,30 +1,29 @@
-const path = require('path');
-var knex = require('knex')({
+const knex = require('knex')({
   client: 'mysql',
   connection: {
-    host : '127.0.0.1',
-    user : 'root',
-    password : '',
-    database : 'recipe_test'
-  }
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'recipe_test',
+  },
 });
 
-var db = require('bookshelf')(knex);
+const db = require('bookshelf')(knex);
 
-//Table Schemas 
-db.knex.schema.hasTable('').then((exists) => {
-  if(!exists) {
-    db.knex.schema.createTable(tableName, function(table) {
-      console.log('I work');
-          //column
-    })
-    .then((table) => console.log('Created Table: ', table));
-  }
-});
+// Table Schemas
+// db.knex.schema.hasTable('').then((exists) => {
+//   if (!exists) {
+//     db.knex.schema.createTable(tableName, function(table) {
+//       console.log('I work');
+//           //column
+//     })
+//     .then((table) => console.log('Created Table: ', table));
+//   }
+// });
 
 db.knex.schema.hasTable('Users').then((exists) => {
-  if(!exists) {
-    db.knex.schema.createTable('Users', function(user) {
+  if (!exists) {
+    db.knex.schema.createTable('Users', (user) => {
       user.increments('id').primary();
       user.string('googleId');
       user.string('googleToken');
@@ -35,13 +34,13 @@ db.knex.schema.hasTable('Users').then((exists) => {
       user.string('zipcode');
       user.timestamps();
     })
-    .then((table) => console.log('Created Table: ', table));
+      .then(table => console.log('Created Table: ', table));
   }
 });
 
 db.knex.schema.hasTable('Businesses').then((exists) => {
-  if(!exists) {
-    db.knex.schema.createTable('Businesses', function(business) {
+  if (!exists) {
+    db.knex.schema.createTable('Businesses', (business) => {
       business.increments('id').primary();
       business.string('name');
       business.string('imgUrl', 100);
@@ -55,28 +54,27 @@ db.knex.schema.hasTable('Businesses').then((exists) => {
       business.integer('distance');
       business.timestamps();
     })
-    .then((table) => console.log('Created Table: ', table));
+      .then(table => console.log('Created Table: ', table));
   }
 });
 
-  db.knex.schema.hasTable('Events').then((exists) => {
-    if(!exists) {
-      db.knex.schema.createTable('Events', function(event) {
-        event.increments('id').primary();
-        event.string('name');
-        event.dateTime('eventDate');
-        event.integer('user_id');
-        event.integer('business_id');
-        event.timestamps();
-      })
-      .then((table) => console.log('Created Table: ', table));
-    }
-  });
-        
+db.knex.schema.hasTable('Events').then((exists) => {
+  if (!exists) {
+    db.knex.schema.createTable('Events', (event) => {
+      event.increments('id').primary();
+      event.string('name');
+      event.dateTime('eventDate');
+      event.integer('user_id');
+      event.integer('business_id');
+      event.timestamps();
+    })
+      .then(table => console.log('Created Table: ', table));
+  }
+});
 
 db.knex.schema.hasTable('LocalFavorites').then((exists) => {
-  if(!exists) {
-    db.knex.schema.createTable('LocalFavorites', function(favorite) {
+  if (!exists) {
+    db.knex.schema.createTable('LocalFavorites', (favorite) => {
       favorite.increments('id').primary();
       favorite.string('zipcode');
       favorite.integer('Italian');
@@ -88,27 +86,27 @@ db.knex.schema.hasTable('LocalFavorites').then((exists) => {
       favorite.integer('Japanese');
       favorite.integer('Mediterranean');
       favorite.integer('French');
-      favorite.integer('Korean');      
+      favorite.integer('Korean');
       favorite.integer('distance');
       favorite.timestamps();
     })
-    .then((table) => console.log('Created Table: ', table));
+      .then(table => console.log('Created Table: ', table));
   }
 });
 
-  db.knex.schema.hasTable('LocalFavorites').then((exists) => {
-    if(!exists) {
-      db.knex.schema.createTable('LocalFavorites', function(favorite) {
-        favorite.increments('id').primary();
-        favorite.integer('spoon_id');
-        favorite.integer('likes');
-        favorite.timestamps();
-      })
-      .then((table) => console.log('Created Table: ', table));
-    }
-  });
+db.knex.schema.hasTable('LocalFavorites').then((exists) => {
+  if (!exists) {
+    db.knex.schema.createTable('LocalFavorites', (favorite) => {
+      favorite.increments('id').primary();
+      favorite.integer('spoon_id');
+      favorite.integer('likes');
+      favorite.timestamps();
+    })
+      .then(table => console.log('Created Table: ', table));
+  }
+});
 
 // ALTER TABLE `Events` ADD FOREIGN KEY (user_id) REFERENCES `User` (`id`);
 // ALTER TABLE `Events` ADD FOREIGN KEY (business_id) REFERENCES `Businesses` (`id`);
-//Schemas above
-module.exports = db; 
+// Schemas above
+module.exports = db;
