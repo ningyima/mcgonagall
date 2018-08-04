20cb3df08be712c9c99ff79c724ac6351d54a8f9
roko <forthrightro@gmail.com>
2018-08-04 11:56:50 -0700

    removed unused mobile section of index file and updated search filters to be compatible with current API source

--- src/app/index.js
+++ src/app/index.js
@@ -99,7 +99,7 @@ class DesktopContainer extends Component {
   }
 
   handleSignupClick () {
-  alert('signup desktop button clicked');
+    alert('signup desktop button clicked');
   }
 
   hideFixedMenu() {
@@ -161,102 +161,10 @@ DesktopContainer.propTypes = {
   children: PropTypes.node,
 }
 
-/*MOBILE*/
-
-class MobileContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-    this.handlePusherClick = this.handlePusherClick.bind(this);
-    this.handleToggle = this.handleToggle.bind(this);
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleSignupClick = this.handleSignupClick.bind(this);
-  }
-
-
-
-  handlePusherClick() {
-    const { sidebarOpened } = this.state
-
-    if (sidebarOpened) this.setState({ sidebarOpened: false })
-  }
-
-  handleToggle() {
-    this.setState({ sidebarOpened: !this.state.sidebarOpened })
-  }
-
-  handleLoginClick (){
-    alert('mobile login button clicked');
-
-  }
-
-  handleSignupClick () {
-  alert('mobile signup btn clicked');
-  }
-
-
-  render() {
-    const { children } = this.props
-    const { sidebarOpened } = this.state
-
-    return (
-      <Responsive maxWidth={Responsive.onlyMobile.maxWidth}>
-        <Sidebar.Pushable>
-          <Sidebar as={Menu} animation='uncover' inverted vertical visible={sidebarOpened}>
-             <Menu.Item>
-                <Image size="mini" src="https://image.flaticon.com/icons/svg/424/424067.svg" />
-                </Menu.Item>
-            <Menu.Item href='#' as='a' active>
-              Home
-            </Menu.Item>
-            <Menu.Item href='#' as='a'>Our Mission</Menu.Item>
-            <Menu.Item href='#' as='a'>Features</Menu.Item>
-            <Menu.Item href='#' as='a'>Benefits</Menu.Item>
-          </Sidebar>
-
-          <Sidebar.Pusher
-            dimmed={sidebarOpened}
-            onClick={this.handlePusherClick}
-            style={{ minHeight: '100vh' }}
-          >
-            <Segment
-              textAlign='center'
-              style={{ minHeight: 350, padding: '1em 0em' }}
-              vertical
-            >
-              <Container>
-                <Menu pointing secondary size='large'>
-                  <Menu.Item onClick={this.handleToggle}>
-                    <Icon name='sidebar' />
-                  </Menu.Item>
-                  <Menu.Item position='right'>
-                  <SearchExampleStandard size='mini' fluid/>
-                    <ModalLoginForm loginHandler={this.handleLoginClick} />
-                    <ModalSignupForm signupHandler={this.handleSignupClick} />
-                  </Menu.Item>
-                </Menu>
-              </Container>
-              <HomepageHeading mobile />
-            </Segment>
-
-            {children}
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
-      </Responsive>
-    )
-  }
-}
-
-MobileContainer.propTypes = {
-  children: PropTypes.node,
-}
 
 const ResponsiveContainer = ({ demoTest, children }) => (
   <div>
-    <DesktopContainer demoTest={demoTest}>{children}</DesktopContainer>
-    <MobileContainer>{children}</MobileContainer>
-  </div>
+    <DesktopContainer demoTest={demoTest}>{children}</DesktopContainer>  </div>
 )
 
 ResponsiveContainer.propTypes = {
