ed1b2a700777cfac15ecfb4ed4ed08a6d93256ae
Ningyi <ningyi6@gmail.com>
2018-08-05 21:58:16 -0700

    add app/index.js

--- src/app/index.js
+++ src/app/index.js
@@ -1,2 +1,29 @@
-Unstaged changes after reset:
-M	src/app/index.js
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import React, { Component } from 'react';
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Responsive,
+  Segment,
+  Sidebar,
+  Visibility,
+} from 'semantic-ui-react';
+
+import DesktopContainer from './DesktopContainer.jsx';
+import MobileContainer from './MobileContainer.jsx';
+
+const HomepageLayout = () => 
+  <div>
+    <DesktopContainer />
+    <MobileContainer />
+  </div>;
+
+export default HomepageLayout;
